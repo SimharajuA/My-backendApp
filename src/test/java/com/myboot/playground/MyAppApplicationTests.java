9e877e46c4d99a451b90ddfb17cabccd1f1cbdad
SimharajuA <adepusimharaju8969@gmail.com>
2022-08-03 15:18:53 +0530

    jira completed

---
+++ src/test/java/com/myboot/playground/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myboot.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
